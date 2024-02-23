0d2c19940ad5a8d9f888021e6e5a200a011b35cf
Rohan Kumar <ohan.Kumar2.Contractor@pepsico.com>
2024-02-23 23:08:18 +0530

    solution to problem 1

--- test/integration/health.test.js
+++ test/integration/health.test.js
@@ -1,7 +1,7 @@
-const request = require('supertest');
-const { app } = require('../../index');
+const request = require("supertest");
+const { app } = require("../../index");
 
-describe('Integration Tests', () => {
+describe("Integration Tests", () => {
   let server;
 
   beforeAll((done) => {
@@ -12,8 +12,8 @@ describe('Integration Tests', () => {
     server.close(done);
   });
 
-  it('should return a 200 OK status code for GET request to /health', async () => {
-    const response = await request(server).get('/health');
+  it("should return a 200 OK status code for GET request to /health", async () => {
+    const response = await request(server).get("/health");
     expect(response.status).toBe(200);
   });
 });
