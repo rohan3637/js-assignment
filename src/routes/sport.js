8964e44cac325d936798ca59590d71b873fe178b
Rohan Kumar <ohan.Kumar2.Contractor@pepsico.com>
2024-02-24 00:50:40 +0530

    problem 2 sol

--- src/routes/sport.js
+++ src/routes/sport.js
@@ -1,11 +1,13 @@
-const Sport = require('../controllers/sport');
+const Sport = require("../controllers/sport");
 
-module.exports = function(app) {
-    app.route('/sport/tour/match').get(async (req, res, next) => {
+module.exports = function (app) {
+    app.route("/sport/tour/match").get(async (req, res, next) => {
         try {
+            const data = await Sport.getAllSportsToursAndMatches();
+            console.log(data);
             return res.json(await Sport.getAllSportsToursAndMatches());
         } catch (err) {
             return next(err);
         }
     });
-}
+};
